5a5387d9d488bc1a616beffe210889436cd5aa3d
BlueAccords <myoldemailname@gmail.com>
2016-06-30 19:48:37 -0700

    added basic even thandling to player

--- src/components/common/Player.js
+++ src/components/common/Player.js
@@ -5,15 +5,26 @@ import * as actions from '../../actions';
 class Player extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleSkip = this.handleSkip.bind(this);
+  }
+
+  handlePlay() {
+    console.log('play was clicked');
+  }
+
+  handleSkip() {
+    console.log('skip was clicked')
   }
 
   render() {
     return (
       <div className="player">
         <div className="player-controls">
-          <svg className="icon icon-backward2"><use xlinkHref="#icon-backward2"></use></svg>
-          <svg className="icon icon-play3"><use xlinkHref="#icon-play3"></use></svg>
-          <svg className="icon icon-forward3"><use xlinkHref="#icon-forward3"></use></svg>
+          <svg className="icon icon-backward2"><use onClick={this.handleSkip} xlinkHref="#icon-backward2"></use></svg>
+          <svg className="icon icon-play3"><use onClick={this.handlePlay} xlinkHref="#icon-play3"></use></svg>
+          <svg className="icon icon-forward3"><use onClick={this.handleSkip} xlinkHref="#icon-forward3"></use></svg>
         </div>
       </div>
     );
