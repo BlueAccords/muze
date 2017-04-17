13a56561e77cffa6dfd4a0fd41fdcb58fab21caf
BlueAccords <myoldemailname@gmail.com>
2017-04-17 12:32:36 -0700

    added example for git merge

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,12 +4,12 @@ ssh -v $DO_USERNAME@$DO_HOSTNAME << EOF
 
 echo '1. Updating sources'
 cd $DO_MUZE_PATH
-git checkout --force master
 git pull
 
 echo "2. Restart PM2 Node process"
-sudo npm install
-sudo pm2 restart $DO_PROCESS
+npm install
+npm run deploy
+pm2 restart $DO_PROCESS
 
 echo $DO_PROCESS
 echo 'Done!'
